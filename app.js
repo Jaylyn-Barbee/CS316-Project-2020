e49a80a50ecb3a5ad6aa6c21e4b05e0943edb638
cait-car <42746268+cait-car@users.noreply.github.com>
2020-11-14 16:58:30 -0500

    added user profile button and added component but not connecting yet

--- app.js
+++ app.js
@@ -10,40 +10,42 @@ const config = {
   measurementId: "G-L0Q3SWCX9L"
 };
 // global access to initialized app database
-var db = firebase.initializeApp(config).database()
+var db = firebase.initializeApp(config).database();
 // global reference to remote data
-var usersRef = db.ref('users')
-var loggedInRef = db.ref('loggedinUser')
-var bgColRef = db.ref('backgroundColor')
-var bgImgRef = db.ref('backgroundImage')
-var ingredientsRef = db.ref('ingredientsList');
-var recipesRef = db.ref('recipesList');
+var usersRef = db.ref("users");
+var loggedInRef = db.ref("loggedinUser");
+var bgColRef = db.ref("backgroundColor");
+var bgImgRef = db.ref("backgroundImage");
+var ingredientsRef = db.ref("ingredientsList");
+var recipesRef = db.ref("recipesList");
 
 // connect Firebase to Vue
-Vue.use(VueFire)
+Vue.use(VueFire);
 //Main Vue instance for the app
 new Vue({
-  el: '#my-app',
+  el: "#my-app",
   data: {
     //these two are not doubling the firebase backgroundColor or backgroundImage bc CSS binding only works on the event.target.value, not the snapshot
-    backgroundColorVal: '',
-    backgroundImageVal: '',
-    newList: '',
+    backgroundColorVal: "",
+    backgroundImageVal: "",
+    newList: "",
     showSignIn: false,
     showRegister: false,
+    showUserProfile: false
   },
   firebase: {
     users: usersRef,
     // binds the below three items as objects instead of as arrays, the firebase default
-    loggedInUser: {source: loggedInRef, asObject: true},
-    backgroundColor: {source: bgColRef, asObject: true},
-    backgroundImage: {source: bgImgRef, asObject: true},
+    loggedInUser: { source: loggedInRef, asObject: true },
+    backgroundColor: { source: bgColRef, asObject: true },
+    backgroundImage: { source: bgImgRef, asObject: true },
     ingredients: ingredientsRef,
-    recipes: recipesRef,
+    recipes: recipesRef
   },
   components: {
-    'login': httpVueLoader('./components/Login.vue'),
-    'register': httpVueLoader('./components/Register.vue'),
+    login: httpVueLoader("./components/Login.vue"),
+    register: httpVueLoader("./components/Register.vue"),
+    userprofile: httpVueLoader("./components/UserProfile.vue")
     /*'add-list': httpVueLoader('AddList.vue'),
     'list': httpVueLoader('List.vue'),
     'categories': httpVueLoader('Categories.vue') */
@@ -55,66 +57,72 @@ new Vue({
   },
   methods: {
     //These methods are pretty straightforward based on the name, just updating firebase values based on user changes
-    createUser(user){
-      this.showRegister = false
-      this.$firebaseRefs.users.push(user)
-      this.$firebaseRefs.loggedInUser.child('name').set(user.name)
-      this.$firebaseRefs.loggedInUser.child('email').set(user.email)
-      this.$firebaseRefs.loggedInUser.child('image').set(user.image)
+    createUser(user) {
+      this.showRegister = false;
+      this.$firebaseRefs.users.push(user);
+      this.$firebaseRefs.loggedInUser.child("name").set(user.name);
+      this.$firebaseRefs.loggedInUser.child("email").set(user.email);
+      this.$firebaseRefs.loggedInUser.child("image").set(user.image);
     },
     openSignIn() {
-      this.showSignIn = true
+      this.showSignIn = true;
     },
     closeSignIn() {
-      this.showSignIn = false
+      this.showSignIn = false;
     },
     openRegister() {
-      this.showRegister = true
+      this.showRegister = true;
     },
     closeRegister() {
-      this.showRegister = false
+      this.showRegister = false;
     },
-    login(user){
-      this.$firebaseRefs.loggedInUser.child('name').set(user.name)
-      this.$firebaseRefs.loggedInUser.child('email').set(user.email)
-      this.$firebaseRefs.loggedInUser.child('image').set(user.image)
-      this.showSignIn = false
+    openUserProfile() {
+      this.showUserProfile = true;
     },
-    logout(){
-      this.$firebaseRefs.loggedInUser.child('name').set('')
-      this.$firebaseRefs.loggedInUser.child('email').set('')
-      this.$firebaseRefs.loggedInUser.child('image').set('')
+    closeUserProfile() {
+      this.showUserProfile = false;
     },
-    backgroundImageUpload(event){
+    login(user) {
+      this.$firebaseRefs.loggedInUser.child("name").set(user.name);
+      this.$firebaseRefs.loggedInUser.child("email").set(user.email);
+      this.$firebaseRefs.loggedInUser.child("image").set(user.image);
+      this.showSignIn = false;
+    },
+    logout() {
+      this.$firebaseRefs.loggedInUser.child("name").set("");
+      this.$firebaseRefs.loggedInUser.child("email").set("");
+      this.$firebaseRefs.loggedInUser.child("image").set("");
+    },
+    backgroundImageUpload(event) {
       //FileReader handles the uploading of the file, when it loads it sets the Firebase background image
-      var file = event.target.files[0]
-      var vm = this
-      var reader = new FileReader()
-      reader.onload = function(e){
-        vm.$firebaseRefs.backgroundImage.set(e.target.result)
-        vm.$firebaseRefs.backgroundColor.set('')
-        vm.backgroundColorVal = ''
-        var image = db.ref('backgroundImage')
-        var imageVal
-        image.on('value', function(snapshot){
-          imageVal = snapshot.val()
-        })
-        vm.backgroundImageVal = imageVal
-        //this.$firebaseRefs.backgroundImage.set(imageVal) doesn't work because the v-style binding doesn't take objects 
-      }
-      reader.readAsDataURL(file)
+      var file = event.target.files[0];
+      var vm = this;
+      var reader = new FileReader();
+      reader.onload = function (e) {
+        vm.$firebaseRefs.backgroundImage.set(e.target.result);
+        vm.$firebaseRefs.backgroundColor.set("");
+        vm.backgroundColorVal = "";
+        var image = db.ref("backgroundImage");
+        var imageVal;
+        image.on("value", function (snapshot) {
+          imageVal = snapshot.val();
+        });
+        vm.backgroundImageVal = imageVal;
+        //this.$firebaseRefs.backgroundImage.set(imageVal) doesn't work because the v-style binding doesn't take objects
+      };
+      reader.readAsDataURL(file);
     },
-    setColor(event){
+    setColor(event) {
       // sets the background color of the project page
-      this.$firebaseRefs.backgroundColor.set(event.target.value)
-      var color = db.ref('backgroundColor')
-      var colorVal
-      color.on('value', function(snapshot){
-        colorVal = snapshot.val()
-      })
-      this.backgroundColorVal = colorVal
-      this.backgroundImageVal = ''
-      this.$firebaseRefs.backgroundImage.set('')
+      this.$firebaseRefs.backgroundColor.set(event.target.value);
+      var color = db.ref("backgroundColor");
+      var colorVal;
+      color.on("value", function (snapshot) {
+        colorVal = snapshot.val();
+      });
+      this.backgroundColorVal = colorVal;
+      this.backgroundImageVal = "";
+      this.$firebaseRefs.backgroundImage.set("");
     }
   }
-})
+});
---
+++ components/UserProfile.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="modal" v-show="show">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h3>User Profile</h3>
+          <button type="button" class="close" @click="closeModal">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <div class="row-fluid">
+            <div class="col-xs-12">
+              <img src="loggedin.image" id="propic" />
+              <p>Name: {{ loggedin.name }}</p>
+              <button class="btn btn-success" @click="edit_name">
+                Edit Name
+              </button>
+              <p>Email: {{ loggedin.email }}</p>
+              <button class="btn btn-success" @click="edit_email">
+                Edit Email
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+module.exports = {
+  data: function () {
+    return {
+      name: "",
+      email: "",
+    };
+  },
+  props: ["users", "show"],
+  methods: {
+    closeModal() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
+
+<style scoped>
+button {
+  margin-top: 1em;
+}
+.modal {
+  display: block;
+}
+.form-row {
+  margin-bottom: 1em;
+}
+</style>
